eef725413975523d1d682b22197a5460f3d75808
Emrah Kinay <emrahkinay@gmail.com>
2026-01-09 20:24:15 -0500

    openai api key fix

--- config.py
+++ config.py
@@ -18,10 +18,15 @@ from open_ai import OpenAIClient
 UPLOAD_API_KEY=os.environ.get("UPLOAD_API_KEY")
 UPLOAD_API_URL=os.environ.get("UPLOAD_API_URL")
 DEEPSEEK_API_KEY=os.environ.get("DEEPSEEK_API_KEY")
-OPEN_AI_API_KEY=os.environ.get("OPEN_AI_API_KEY")
+OPEN_AI_API_KEY=os.environ.get("OPEN_AI_API_KEY") or os.environ.get("OPENAI_API_KEY")
 
-# LLM
-LLM_API_CLIENT=OpenAIClient( api_key = OPEN_AI_API_KEY )
+if OPEN_AI_API_KEY:
+    LLM_API_CLIENT=OpenAIClient( api_key = OPEN_AI_API_KEY )
+else:
+    raise ValueError(
+        "OPEN_AI_API_KEY or OPENAI_API_KEY environment variable must be set. "
+        "Please set it in your GitHub Actions secrets or environment variables."
+    )
 
 # URLS
 FACILITIES_LIST_URL='https://ottawa.ca/en/recreation-and-parks/recreation-facilities/place-listing'
